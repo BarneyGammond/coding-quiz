419aef6a262b702f581ee14801b533e2b15bf403
Barney Gammond <barney@gammond.co.uk>
2020-04-27 17:33:12 +0100

    Updates

---
+++ precache-manifest.585332e51f9fb4e0c323f0445a15d0f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8734cdd0ec15c21c0e168188317121a",
+    "url": "/coding-quiz/index.html"
+  },
+  {
+    "revision": "23155f8b4ac3737006fb",
+    "url": "/coding-quiz/static/css/main.acc6c0f6.chunk.css"
+  },
+  {
+    "revision": "8bf5b375623891bf4c7f",
+    "url": "/coding-quiz/static/js/2.41e4cb12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coding-quiz/static/js/2.41e4cb12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23155f8b4ac3737006fb",
+    "url": "/coding-quiz/static/js/main.10e19dd2.chunk.js"
+  },
+  {
+    "revision": "87db8b9515e0d922a3bb",
+    "url": "/coding-quiz/static/js/runtime-main.0562794c.js"
+  }
+]);
